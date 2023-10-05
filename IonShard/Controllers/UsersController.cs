ce13d77a9649affd2b45f3a83791b17946cf66f6
EdvardfFH <Carbondubrasil@gmail.com>
2023-10-05 09:24:04 +0200

    It is now possible to change unit location

--- IonShard/Controllers/UsersController.cs
+++ IonShard/Controllers/UsersController.cs
@@ -1,13 +1,14 @@
-﻿using IonShard.Domain.Units;
+﻿using IonShard.Domain.Map;
+using IonShard.Domain.Units;
 using IonShard.Domain.Users;
 using IonShard.DTO.Units;
 using IonShard.DTO.Users;
 using IonShard.Mappers;
 using IonShard.Services;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Text.RegularExpressions;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace IonShard.Controllers;
 
@@ -19,17 +20,20 @@ public class UsersController : ControllerBase
     
     private readonly UsersRepository _usersRepository;
     private readonly UserFactory _userFactory;
+    private readonly MapRepository _mapRepository;
 
-    public UsersController(UsersRepository usersRepository, UserFactory userFactory)
+    public UsersController(UsersRepository usersRepository, UserFactory userFactory, MapRepository mapRepository)
     {
         _usersRepository = usersRepository;
         _userFactory = userFactory;
+        _mapRepository = mapRepository;
     }
 
 
     [HttpPut("{userId}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [SwaggerOperation(Summary = "Create a new user")]
     public ActionResult<UserDTO?> CreateNewUser(string userId, [FromBody] CreateUserPutRequestBody body)
     {
         if (body.Id is not null && body.Pseudo is not null && body.Id == userId
@@ -49,6 +53,7 @@ public class UsersController : ControllerBase
     [HttpGet("{userId}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [SwaggerOperation(Summary = "Returns details of an existing user")]
     public ActionResult<UserDTO> GetUser(string userId)
     {
         UserDTO? user = _usersRepository[userId]?.ToDTO();
@@ -62,6 +67,7 @@ public class UsersController : ControllerBase
     [HttpGet("{userId}/units")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [SwaggerOperation(Summary = "Returns all units of a user")]
     public ActionResult<IEnumerable<UnitDTO>> GetAllUnitsOfUser(string userId)
     {
         User? user = _usersRepository[userId];
@@ -78,6 +84,7 @@ public class UsersController : ControllerBase
     [HttpGet("{userId}/units/{unitId}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [SwaggerOperation(Summary = "Returns information about one unit")]
     public ActionResult<UnitDTO> GetOneUnitFromUser(string userId, string unitId)
     {
         Unit? unit = GetUnitFromRepository(userId, unitId);
@@ -92,18 +99,29 @@ public class UsersController : ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public ActionResult<Unit?> MoveUnitOfUser(string userId, string unitId, [FromBody] MoveUnitPutRequestBody body)
+    public ActionResult<UnitDTO?> MoveUnitOfUser(string userId, string unitId, [FromBody] MoveUnitPutRequestBody body)
     {
-        if (unitId == body.Id && body.Id is not null && body.System is not null)
-        {
-            Unit unit = GetUnitFromRepository(userId, unitId);
-
-            return null; // todo: finish method
-        }
-        else
-        {
+        if (unitId != body.Id || body.Id is null || body.System is null)
             return BadRequest();
-        }
+
+        Unit? unit = GetUnitFromRepository(userId, unitId);
+        Location? location = unit?.Location;
+        StarSystem? system = _mapRepository[body.System];
+            
+        if (unit is null || location is null || system is null)
+            return NotFound();
+            
+        Planet? planet = body.Planet is not null
+            ? system?[body.Planet]
+            : null;
+
+        if (planet is null && body.Planet is not null)
+            return NotFound();
+
+        location.System = system;
+        location.Planet = planet;
+
+        return unit.ToDTO();
     }
 
 
