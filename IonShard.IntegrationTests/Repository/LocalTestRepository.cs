c7505a24e45840748e6132b71113ffdf6b0496f2
JeanMomo <saavedra-arthur@live.fr>
2023-10-25 20:03:56 +0200

    init xunit tests

---
+++ IonShard.IntegrationTests/Repository/LocalTestRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using IonShard.Domain.Map;
+using IonShard.Persistence.Repositories;
+
+namespace IonShard.IntegrationTests.Repository;
+
+public class LocalTestRepository
+{
+    private const string JsonFilePathName = "../../../TestResources/TestRepoSystems.json";
+    private static LocalTestRepository _instance;
+    private Universe _universe;
+    private BuildingRepository _buildingRepository;
+    private UserRepository _userRepository;
+
+    private LocalTestRepository(List<StarSystem> systems)
+    {
+        _universe = new Universe(systems);
+        _buildingRepository = new BuildingRepository();
+        _userRepository = new UserRepository();
+    }
+
+    public StarSystem? this[string systemName] => _universe[systemName];
+
+    public static LocalTestRepository GetInstance()
+    {
+        if (_instance is null)
+        {
+            string jsonString = File.ReadAllText(JsonFilePathName);
+            List<StarSystem> systems = JsonSerializer.Deserialize<List<StarSystem>>(jsonString)!;
+            _instance = new LocalTestRepository(systems);
+        }
+
+        return _instance;
+    }
+}
