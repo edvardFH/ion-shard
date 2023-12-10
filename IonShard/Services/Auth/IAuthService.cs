0fedec6287773fb88419d438c34ec87bbac136c3
JeanMomo <saavedra-arthur@live.fr>
2023-12-10 17:28:23 +0100

    refacto authentication handler

---
+++ IonShard/Services/Auth/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿using IonShard.Configuration.Auth;
+
+namespace IonShard.Services.Auth;
+
+public interface IAuthService
+{
+    public AuthUserEntity? Authenticate(string username, string password);
+}
