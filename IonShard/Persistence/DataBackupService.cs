917b1af37c94d6ca642525fd0ad702704a96e6e0
edvardFH <97302653+edvardFH@users.noreply.github.com>
2024-01-16 21:34:03 +0100

    Backup works (docker image has to be rebuilt after code modifications

--- IonShard/Persistence/DataBackupService.cs
+++ IonShard/Persistence/DataBackupService.cs
@@ -1,21 +1,39 @@
 ﻿using IonShard.Persistence.Database;
 using IonShard.Persistence.Repositories;
+using Shard.Shared.Core;
 
 namespace IonShard.Persistence;
 
 public class DataBackupService: IDataBackupService
 {
+    private const int BackupPeriod = 60;
+    private const int DueTime = 50;
+
     private readonly MapRepository _mapRepository;
     private readonly IShardDatabaseService _database;
 
-    public DataBackupService(MapRepository mapRepository, IShardDatabaseService database)
+    private readonly ITimer _timer;
+
+
+    public DataBackupService(MapRepository mapRepository, IShardDatabaseService database, IClock clock)
     {
         _mapRepository = mapRepository;
         _database = database;
+
+        var period = TimeSpan.FromSeconds(BackupPeriod);
+
+        _timer = clock.CreateTimer
+            (
+                (state) => BackupData(),
+                null,
+                TimeSpan.FromSeconds(DueTime),
+                TimeSpan.FromSeconds(BackupPeriod)
+            );
     }
 
     public void BackupData()
     {
-        _database.UpdateMap(_mapRepository.Systems);
+        _database.UpdateMapAsync(_mapRepository.Systems);
+        Console.WriteLine("Backup done.");
     }
 }
